798e27dddf7000785c299c52f69bfe5287fd5b0f
Daniel Greil <danimickigreil@gmail.com>
2022-09-08 10:35:46 +0200

    Project has been put on hold

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-FieldPositions = [
-    't-l', 't-m', 't-r',
-    'm-l', 'm-m', 'm-r',
-    'b-l', 'b-m', 'b-r',
-]
-
-TheoreticalFieldPositions = [
+AllFields = [
     't-l', 't-m', 't-r',
     'm-l', 'm-m', 'm-r',
     'b-l', 'b-m', 'b-r',
@@ -13,6 +7,7 @@ TheoreticalFieldPositions = [
 UserPlayer = {
 PlayerIcon: 'X',
 Positions: [],
+PotentialPosition: [],
 Counter: 0,
 WinningValue: 1,
 }
@@ -20,20 +15,22 @@ WinningValue: 1,
 Machine = {
 PlayerIcon: 'O',
 Positions: [],
+PotentialPosition: [],
 Counter: 0,
 WinningValue: -1,
 }
 
 GameEnded = false
-
+PotentialGameEnded = false
 
 // Place the Icon on said Field
-function PlaceInPosition(Player, Position) {
+function Place(Player, Position) {
     document.getElementById(Position).innerHTML = Player.PlayerIcon;
     document.getElementById(Position).setAttribute("disabled", "true")
 
-    Player.Positions.push(FieldPositions.splice(FieldPositions.indexOf(Position), 1).join());
+    Player.Positions.push(AllFields.splice(AllFields.indexOf(Position), 1).join());
 }
+
 
 // Check if one Party has wont the game
 function CheckGameStatus(Player) {
@@ -56,6 +53,7 @@ function CheckGameStatus(Player) {
             EndGame();
 
             FieldsThatWon = AllWinCon[i]
+            
             for (let a = 0; a < FieldsThatWon.length; a++) {
                 document.getElementById(FieldsThatWon[a]).style.backgroundColor = "lightgreen";
             }
@@ -65,32 +63,7 @@ function CheckGameStatus(Player) {
     }
 }
 
-function checkTurn(Player, position) {
-    // We will have to check for every winning condition manually
-    let WinCon1 = ['t-l', 't-m', 't-r'] // Horizontal
-    let WinCon2 = ['m-l', 'm-m', 'm-r']
-    let WinCon3 = ['b-l', 'b-m', 'b-r']
-    
-    let WinCon4 = ['t-l', 'm-l', 'b-l'] // Vertical
-    let WinCon5 = ['t-m', 'm-m', 'b-m']
-    let WinCon6 = ['t-r', 'm-r', 'b-r']
-    
-    let WinCon7 = ['t-l', 'm-m', 'b-r'] // Diagonal
-    let WinCon8 = ['t-r', 'm-m', 'b-l']
-    let AllWinCon = [WinCon1, WinCon2, WinCon3, WinCon4, WinCon5, WinCon6, WinCon7, WinCon8]
 
-    // Define how the array will look like after next move
-    PossibleTurn = Player.Positions;
-    PossibleTurn.push(position);
-
-    // Check for every possible winning condition
-    for (let i = 0; i < AllWinCon.length; i++) {
-        if (AllWinCon[i].every(elem => Player.Positions.includes(elem))) {
-            
-        }
-    }
-    return false
-}
 
 function EndGame() {
     GameEnded = true
@@ -105,7 +78,7 @@ function EndGame() {
 function resetField() {
     GameEnded = false
 
-    FieldPositions = [
+    AllFields = [
         't-l', 't-m', 't-r',
         'm-l', 'm-m', 'm-r',
         'b-l', 'b-m', 'b-r',
@@ -123,14 +96,17 @@ function resetField() {
 
 // Recieve the User Input and Validate it
 function UserInput(InputString) {
-    PlaceInPosition(UserPlayer, InputString)
+    Place(UserPlayer, InputString)
 
     CheckGameStatus(UserPlayer)
     
-    if (FieldPositions.length != 0) {
+    if (AllFields.length != 0) {
         TurnMachine()
     }
 }
+
+
+
 
 // Let the Machine decide what turn it wants to do
 function TurnMachine() {
@@ -138,56 +114,129 @@ function TurnMachine() {
 
         if (document.getElementById('easy').checked) {
             // The game is set to easy
-            ChosenPosition = FieldPositions[Math.floor(Math.random() * (FieldPositions.length - 1))];
-            PlaceInPosition(Machine, ChosenPosition)
+            ChosenPosition = AllFields[Math.floor(Math.random() * (AllFields.length - 1))];
+            Place(Machine, ChosenPosition)
             CheckGameStatus(Machine)
 
         } else {
             // The game is set to hard
-
-            // Simulate possible game development
-
-            PossibleSteps()
-            
-            // Reset the array
-            MoveArray = []
-
+            ChosenPosition = NextAiMove();
+            Place(Machine, ChosenPosition)
+            CheckGameStatus(Machine)
         }
     }
 }
 
-MoveArray = []
+function NextAiMove() {
+    while (true) {
+        // Vars for a Fresh Simulation
+        let SimAllFields = AllFields
+        let MoveArray = []
+        let WinningArray = []
+
+
+        for (let i = 0; i < SimAllFields.length; i++) {
+            rndField = SimAllFields[Math.floor(Math.random() * (SimAllFields.length - 1))]
+            MoveArray.push(rndField)
+            console.log(rndField)
+            TheoreticalPlace(SimAllFields, Machine, rndField)
+            console.log("Placed in: " + rndField + ", " + SimAllFields.length + " left")
+
+            if (checkTurn(Machine)) {
+                WinningArray = MoveArray
+                console.log("Machine would win")
+                break;
+            }
+            
+
+
+            rndField = SimAllFields[Math.floor(Math.random() * (SimAllFields.length - 1))]
+            TheoreticalPlace(SimAllFields, UserPlayer, rndField)
+        }
+
+        if (WinningArray != []) {
+            return WinningArray[0];
+        }
+    }
+}
+
+
+TheoreticalAllFields = [
+    't-l', 't-m', 't-r',
+    'm-l', 'm-m', 'm-r',
+    'b-l', 'b-m', 'b-r',
+]
+
+function TheoreticalPlace(Field, Player, Position) {
+    Player.PotentialPosition.push(Field.splice(Field.indexOf(Position), 1).join());
+}
+
+
+function checkTurn(Player) {
+    // We will have to check for every winning condition manually
+    let WinCon1 = ['t-l', 't-m', 't-r'] // Horizontal
+    let WinCon2 = ['m-l', 'm-m', 'm-r']
+    let WinCon3 = ['b-l', 'b-m', 'b-r']
+    
+    let WinCon4 = ['t-l', 'm-l', 'b-l'] // Vertical
+    let WinCon5 = ['t-m', 'm-m', 'b-m']
+    let WinCon6 = ['t-r', 'm-r', 'b-r']
+    
+    let WinCon7 = ['t-l', 'm-m', 'b-r'] // Diagonal
+    let WinCon8 = ['t-r', 'm-m', 'b-l']
+    let AllWinCon = [WinCon1, WinCon2, WinCon3, WinCon4, WinCon5, WinCon6, WinCon7, WinCon8]
+
+    // Check for every possible winning condition
+    for (let i = 0; i < AllWinCon.length; i++) {
+        if (AllWinCon[i].every(elem => Player.PotentialPosition.includes(elem))) {
+            PotentialGameEnded = true
+            FieldsThatWon = AllWinCon[i]
+
+            // Player Won
+            return true;
+        }
+    }
+    return false;
+}
 
 // Function to explore all possible moves
 function PossibleSteps() {
-    while (FieldPositions.length > 0 && GameEnded == false) {
+    TheoreticalAllFields = AllFields
+    while (TheoreticalAllFields.length > 0 && PotentialGameEnded == false) {
 
                 
-        if (FieldPositions.length > 0 && GameEnded == false) {
-            console.log(MakeAStep(Machine))
+        if (TheoreticalAllFields.length > 0 && PotentialGameEnded == false) {
+            let Result = MakeARotation(Machine)
+            if (Result == -1) {
+                WinningPossibilities.push(MoveArray);
+                console.log("Machine Won")
+            }
         }
         
-        if (FieldPositions.length > 0 && GameEnded == false) {
-            console.log(MakeAStep(UserPlayer))
+        if (TheoreticalAllFields.length > 0 && PotentialGameEnded == false) {
+            let Result = MakeARotation(UserPlayer)
+            if (Result == 1) {
+                LoosingPossibilities.push(MoveArray);
+                console.log("Player Won")
+            }
         }
-
 
     }
     console.log(MoveArray)
 }
 
 // Simulates a move of a player
-function MakeAStep(Player) {
-    rndField = FieldPositions[Math.floor(Math.random() * (FieldPositions.length - 1))]
-    PlaceInPosition(Player, rndField)
+function MakeARotation(Player) {
+    rndField = TheoreticalAllFields[Math.floor(Math.random() * (TheoreticalAllFields.length - 1))]
+    TheoreticalPlace(Player, rndField)
 
     if (Player.WinningValue == -1) {
         // This is only for the bot
         MoveArray.push(rndField);
     }
     
-    
-    if (CheckGameStatus(Player)) {
+
+    if (checkTurn(Player)) {
         return Player.WinningValue;
     } else {
         return "";
